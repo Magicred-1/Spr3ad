65fdaa558486a5f9b8ab467ec368ee25e0fe087b
Anthony ISSA <anthony.issa@hotmail.fr>
2024-07-13 13:00:21 +0200

    the graph

--- FrontEnd/components/onboarding.tsx
+++ FrontEnd/components/onboarding.tsx
@@ -17,44 +17,74 @@ function Onboarding() {
   >([]);
   const { setShowLinkNewWalletModal } = useDynamicModals();
   const userWallets = useUserWallets();
+  const [lastSeenWallets, setLastSeenWallets] = useState<string[]>([]);
 
-  const getUserTags = async () => {
-    const nbTags = TAGS.length;
-    const tags = shuffleTags();
-    const tagsOfUser = [];
-    const nbUserTags = Math.floor(Math.random() * nbTags) + 1;
-    for (let i = 0; i < nbUserTags; i++) {
-      tagsOfUser.push(tags[i]);
+  const isThereNewWallets = () => {
+    return userWallets.some(
+      (wallet) => !lastSeenWallets.includes(wallet.address)
+    );
+  };
+
+  const getUserTags = () => {
+    if (!isThereNewWallets()) {
+      return;
+    }
+    setLastSeenWallets(userWallets.map((wallet) => wallet.address));
+    const shuffledTags = shuffleTags();
+    const tagsPerWallet = 2;
+    const newTags = [];
+
+    for (let i = 0; i < userWallets.length; i++) {
+      for (let j = 0; j < tagsPerWallet; j++) {
+        const tagIndex = (i * tagsPerWallet + j) % shuffledTags.length;
+        newTags.push(shuffledTags[tagIndex]);
+      }
     }
 
-    setUserTags(tagsOfUser);
+    const combinedTags = [...userTags, ...newTags];
+    const uniqueTags = Array.from(new Set(combinedTags.map((tag) => tag.name)))
+      .map((name) => combinedTags.find((tag) => tag.name === name))
+      .filter(
+        (tag): tag is { name: string; description: string; image: string } =>
+          tag !== undefined
+      );
+
+    setUserTags(uniqueTags);
   };
 
   useEffect(() => {
     getUserTags();
-  }, []);
+  }, [userWallets]);
+
+  const setup = () => {
+    //TODO
+  };
 
   return (
     <div className="mx-auto text-center flex flex-col gap-10">
-      <h1 className="text-3xl font-bold">Onboarding</h1>
+      <h1 className="text-3xl font-bold">Get Started</h1>
       <div className="link-wallet-container">
         <Button
           className="profile-button"
+          variant={"secondary"}
           onClick={() => setShowLinkNewWalletModal(true)}
         >
           Link New Wallet
         </Button>
       </div>
+      <Button className="w-64 mx-auto" onClick={setup}>
+        Enter Spread
+      </Button>
       {userTags.length > 0 && (
         <div>
           <h2 className="text-2xl font-bold mb-3">
             We Found {userTags.length} Tags for You
           </h2>
-          <ul className="flex gap-5 w-10/12 justify-center mx-auto">
+          <ul className="flex gap-5 w-10/12 justify-center mx-auto flex-wrap">
             {userTags.map((tag) => (
               <li
                 key={tag.name}
-                className="flex flex-col justify-between items-center  bg-purple-500/5 p-3 rounded-xl"
+                className="flex flex-col justify-between items-center w-32 bg-purple-500/5 p-3 rounded-xl"
               >
                 <img
                   src={tag.image}
@@ -80,8 +110,6 @@ function Onboarding() {
           </ul>
         </div>
       )}
-
-      <Button>Enter Spread</Button>
 
       <DynamicMultiWalletPromptsWidget />
     </div>
--- FrontEnd/components/utils/tags.ts
+++ FrontEnd/components/utils/tags.ts
@@ -10,41 +10,49 @@ export const shuffleTags = () => {
 export const TAGS = [
     {
         name: "Apecoin",
+        tag: "apecoin",
         description: "You own Apecoin.",
-        image: "https://apecoin.com/assets/wordmark.svg"
+        image: "https://research.binance.com/static/images/projects/apecoin/logo.png"
     },
     {
         name: 'POAP ETH London',
+        tag: 'poap-eth-london',
         description: 'You attended EthGlobal London.',
         image: "https://assets.poap.xyz/4903e4a8-64c0-470f-8873-2cf49ec6482f.png"
     },
     {
         name: "Artist",
+        tag: "artist",
         description: "You created your own collection with more than 1 ETH in volume.",
-        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thecut.com%2F2016%2F01%2Fartists-way-self-help-book.html&psig=AOvVaw2mqrhAfA9Q0_ifrCoZ5z5o&ust=1720916563076000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjc7fXfoocDFQAAAAAdAAAAABAE"
+        image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQrUJYpcs_FFt1tZNeCngdywx2OLcQAygbiPNLXj1p10pWCFe5EPlJlLK9U8zUWiIqpRCKZbCvWfEs8NA21xDc1STD9M8y7sZN44IRW5A"
     },
     {
         name: "DeFi Trader",
+        tag: "defi-trader",
         description: "You traded more than 1 ETH in DeFi.",
         image: "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/DLJW7BAKPFEG3HCSNWPYV4QBWQ.png"
     },
     {
         name: "Lens User",
+        category: "lens",
         description: "You own a Lens Profile.",
         image: "https://img.cryptorank.io/coins/lens_protocol1667554440648.png"
     },
     {
         name: "Farcaster",
+        code: "farcaster",
         description: "You own a Farcaster Profile.",
         image: "https://chainbroker.io/_next/image/?url=https%3A%2F%2Fstatic.chainbroker.io%2Fmediafiles%2Fprojects%2Ffarcaster%2Ffarcaster.jpeg&w=2560&q=75"
     },
     {
         name: "Blast User",
+        code: "blast",
         description: "You made more than 20 transactions on Blast.",
         image: "https://cdn.prod.website-files.com/614c99cf4f23700c8aa3752a/66425fa482bce3bc3cb20b41_blast_blockchain_logo.jpeg",
     },
     {
         name: "The Graph User",
+        code: "the-graph",
         description: "You deposited GRT on the Graph.",
         image: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Logo_of_The_Graph.jpg"
     },
